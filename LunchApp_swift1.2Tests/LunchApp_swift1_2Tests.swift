57c39de64432ba8fe10dd11b49202f64b287530f
byGeek01 <hide_69ch@yahoo.co.jp>
2015-06-22 18:48:08 +0900

    Initial Commit

---
+++ LunchApp_swift1.2Tests/LunchApp_swift1_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  LunchApp_swift1_2Tests.swift
+//  LunchApp_swift1.2Tests
+//
+//  Created by hide on 2015/06/22.
+//  Copyright (c) 2015年 byGeek01. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class LunchApp_swift1_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
